f11c18d3591f302b71cf2facaaa4a7115598d1d5
weterynarzfred <weterynarzfred@gmail.com>
2020-07-21 12:39:05 +0200

    correct folders

--- functions.php
+++ functions.php
@@ -6,14 +6,13 @@ function viewport_meta() {?>
 <meta name="viewport" content="width=device-width, initial-scale=1.0" /><?php }
   add_filter('wp_head', 'viewport_meta');
 
-  function f3_enqueue() {
+  add_action('wp_enqueue_scripts', function() {
     wp_enqueue_style('style', get_template_directory_uri() . '/style.css', 1.0);
-    wp_enqueue_style('base', get_template_directory_uri() . '/css/base.css', array('style'), 1.0);
+    wp_enqueue_style('base', get_template_directory_uri() . '/dist/css/base.css', array('style'), 1.0);
 
     wp_enqueue_script('jquery');
-    wp_enqueue_script('f3', get_template_directory_uri() . '/js/f3.js', array('jquery'), 1.0, true);
-  }
-  add_action('wp_enqueue_scripts', 'f3_enqueue');
+    wp_enqueue_script('f3', get_template_directory_uri() . '/dist/js/f3.js', array('jquery'), 1.0, true);
+  });
 
   function remove_menus() {
     remove_menu_page('edit.php'); //Posts
