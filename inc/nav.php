0947cc67cff8654c51bbf8efa74f2680ec6309d8
weterynarzfred <weterynarzfred@gmail.com>
2020-07-23 16:47:15 +0200

    nav menu

--- inc/nav.php
+++ inc/nav.php
@@ -18,4 +18,25 @@
     <div></div>
     <div></div>
   </div>
+  <div id="nav-overlay">
+    <div class=""></div>
+    <div class=""></div>
+    <div id="nav-o-links">
+      <div class="nav-link"><span>nasza idea</span></div>
+      <div class="nav-link"><span>villa</span></div>
+      <div class="nav-link"><span>domki nad stawem</span></div>
+      <div class="nav-link"><span>relaks i spa</span></div>
+      <div class="nav-link"><span>restauracja</span></div>
+      <div class="nav-link"><span>atrakcje</span></div>
+      <div class="nav-link"><span>kontakt</span></div>
+    </div>
+    <div id="nav-book">rezerwuj</div>
+    <div id="nav-foot">
+      <div class="nav-foot-col">+48 897 978 857</div>
+      <div class="nav-foot-col">kontakt@lesnakapiel.com</div>
+      <div class="nav-foot-col empty"></div>
+      <div class="nav-foot-col">ul. Karpacka 1, Karpaty</div>
+      <div class="nav-foot-col social">Ea, eaque</div>
+    </div>
+  </div>
 </nav>
--- inc/top.php
+++ inc/top.php
@@ -2,7 +2,7 @@
   $img = get_field('top_image');
 ?>
 
-<div id="top" class="content">
+<div id="top">
   <div id="top-bg" style="background-image: url('<?=$img['sizes']['large']?>')">
   </div>
 </div>
