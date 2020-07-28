eee62cdb8b8d76339bb39792c17f237b23efe519
weterynarzfred <weterynarzfred@gmail.com>
2020-07-28 16:08:41 +0200

    kontakt

--- inc/contact-images.php
+++ inc/contact-images.php
@@ -1,14 +1,15 @@
 <?php
-$images= get_field('images');
+  $images = get_field('images');
 ?>
 
 <div class="content-wide">
   <div class="column-outer flex flex-wrap">
-    <?php foreach ($images as $image) { ?>
+    <?php foreach ($images as $image) {?>
     <div class="column-inner col4">
       <div class="cake"
-        style="background-image: url(<?=$image['sizes']['medium']?>);"></div>
+        style="background-image: url(<?=$image['image']['sizes']['medium']?>);">
+      </div>
     </div>
-    <?php } ?>
+    <?php }?>
   </div>
 </div>
