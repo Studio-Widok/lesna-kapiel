25c28f534e727335ba8e1f77b49e9799cdfe8d9e
wojciechlasak <wojciech.lasak@outlook.com>
2020-08-17 14:59:38 +0200

    get attraction by tag, get collection by category

--- templates/t-atrakcje.php
+++ templates/t-atrakcje.php
@@ -1,6 +1,27 @@
 <?php /*Template Name: atrakcje*/
   get_header();
-?>
+
+  $attraction = $_GET['attraction']; //explode(',', $_GET['attraction']);
+
+  $posts = get_posts(array(
+    'numberposts' => -1,
+    'post_type'   => 'apartment',
+    'order'       => 'ASC',
+    'tax_query'   => [
+      [
+        'taxonomy' => 'apartment_tags',
+        'field'    => 'slug',
+        'terms'    => $attraction,
+      ],
+    ],
+  ));
+
+  foreach ($posts as $post): setup_postdata($post);
+  ?>
+<div><?=get_the_title()?></div>
+<?php
+    endforeach
+  ?>
 
 
 <?php get_footer();?>
--- templates/t-kolekcje.php
+++ templates/t-kolekcje.php
@@ -1,6 +1,26 @@
 <?php /*Template Name: kolekcje*/
   get_header();
-?>
 
+  $collection = $_GET["attraction"];
+
+  $posts = get_posts(array(
+    'numberposts' => -1,
+    'post_type'   => 'apartment',
+    'order'       => 'ASC',
+    'tax_query'   => [
+      [
+        'taxonomy' => 'apartment_categories',
+        'field'    => 'slug',
+        'terms'    => $collection,
+      ],
+    ],
+  ));
+
+  foreach ($posts as $post): setup_postdata($post);
+  ?>
+<div><?=get_the_title()?></div>
+<?php
+    endforeach
+  ?>
 
 <?php get_footer();?>
