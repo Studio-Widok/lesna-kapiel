af46ae1abc2237c6379fff3e7ce4429cd86876dc
wojciechlasak <wojciech.lasak@outlook.com>
2020-08-17 17:08:31 +0200

    added fixed-link

---
+++ components/fixed-link.php
@@ -0,0 +1,10 @@
+<?php
+  $text = $text ?? '';
+  $link = $link ?? '';
+?>
+
+<div class="fixed-link flex flex-column flex-align-center">
+  <a href="<?=$link?>">
+    <span><?=$text?></span>
+  </a>
+</div>
