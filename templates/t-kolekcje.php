ed717329ba6e44361780f017097f70657563b790
wojciechlasak <wojciech.lasak@outlook.com>
2020-08-18 09:14:16 +0200

    added comment

--- templates/t-kolekcje.php
+++ templates/t-kolekcje.php
@@ -16,8 +16,31 @@
     ],
   ));
 
-  foreach ($posts as $post): setup_postdata($post);
-  ?>
+  // dont know where to put that
+//   $attraction = $_GET['attraction'];
+
+//   $posts = get_posts(array(
+//     'numberposts' => -1,
+//     'post_type'   => 'apartment',
+//     'order'       => 'DSC',
+//     'tax_query'   => [
+//       [
+//         'taxonomy' => 'apartment_tags',
+//         'field'    => 'slug',
+//         'terms'    => $attraction,
+//       ],
+//     ],
+//   ));
+
+//   foreach ($posts as $post): setup_postdata($post);
+//   ?>
+// <div><?=get_the_title()?></div>
+// <?php
+//     endforeach
+//   ?>
+
+foreach ($posts as $post): setup_postdata($post);
+?>
 <div><?=get_the_title()?></div>
 <?php
     endforeach
