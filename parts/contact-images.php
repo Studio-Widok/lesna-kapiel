537d5eb24898558ff253dd02300a6a6b0006de78
wojciechlasak <wojciech.lasak@outlook.com>
2020-08-24 11:47:34 +0200

    added instagram-feed

--- parts/contact-images.php
+++ parts/contact-images.php
@@ -3,8 +3,12 @@
 ?>
 
 <div class="content-wide">
-  <div class="column-outer flex flex-wrap">
-    <?php foreach ($images as $image) {?>
+  <div class="instagram-container column-outer flex flex-wrap">
+    <?php
+      $instagram = do_shortcode('[instagram-feed]');
+    ?>
+    <?=$instagram?>
+<?php foreach ($images as $image) {?>
     <div class="column-inner col4">
       <div class="cake"
         style="background-image: url(<?=$image['image']['sizes']['medium']?>);">
