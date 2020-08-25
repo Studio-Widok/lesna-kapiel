1378d133ecbf07ded79b90aba722f207901e1a09
wojciechlasak <wojciech.lasak@outlook.com>
2020-08-25 10:53:25 +0200

    added map and fixed-link

--- parts/contact-images.php
+++ parts/contact-images.php
@@ -8,12 +8,5 @@
       $instagram = do_shortcode('[instagram-feed]');
     ?>
     <?=$instagram?>
-<?php foreach ($images as $image) {?>
-    <div class="column-inner col4">
-      <div class="cake"
-        style="background-image: url(<?=$image['image']['sizes']['medium']?>);">
-      </div>
-    </div>
-    <?php }?>
   </div>
 </div>
