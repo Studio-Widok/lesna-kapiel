9cffb761f961191bf695d6bf5f2dbd50b8b6f014
wojciechlasak <wojciech.lasak@outlook.com>
2020-08-26 10:37:32 +0200

    decreased font-size,  rotate -90

--- components/fixed-link.php
+++ components/fixed-link.php
@@ -5,6 +5,6 @@
 
 <div class="fixed-link flex flex-column flex-align-center">
   <a href="<?=$link?>">
-    <span class="uppercase"><?=$text?></span>
+    <span class="uppercase small"><?=$text?></span>
   </a>
 </div>
