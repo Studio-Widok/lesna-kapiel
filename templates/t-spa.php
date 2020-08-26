a258b64b8fa0254d3f692b0b906dc6747e3003f8
wojciechlasak <wojciech.lasak@outlook.com>
2020-08-26 13:09:49 +0200

    added text_align, change img -> bg

--- templates/t-spa.php
+++ templates/t-spa.php
@@ -2,16 +2,19 @@
   get_header();
 
   $sections = get_field('sections');
+  $top      = get_field('top');
 
   get_part('nav');
   get_part('top', array(
     'show_title' => true,
+    'bg'         => $top['top_image'],
+    'text_align' => $top['align'],
   ));
 ?>
 
 <div class="green-wrapper">
   <div class="rsep"></div>
-  <?php get_part('text-full', array('text' => get_field('text')));?>
+  <?php get_part('text-full', array('text' => get_field('top')['text']));?>
 
   <?php for ($i = 0; $i < count($sections); $i++) {?>
 
@@ -31,39 +34,6 @@
   }?>
 
 
-  <div class="rsep"></div>
-</div>
-
-<div class="green-wrapper fixed-link-container">
-  <?php get_component('fixed-link', array('text' => 'babababa', 'link' => 'a'));?>
-  <div class="rsep"></div>
-  <div class="rsep"></div>
-  <div class="rsep"></div>
-  <div class="rsep"></div>
-  <div class="rsep"></div>
-  <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius optio, iste
-    debitis maxime ut doloremque nam tempore est, fuga distinctio asperiores
-    ipsa dolor amet totam magnam eaque ex quasi aperiam.</p>
-  <div class="rsep"></div>
-</div>
-
-<div class="green-wrapper fixed-link-container">
-  <?php get_component('fixed-link', array('text' => 'lalaalal', 'link' => 'a'));?>
-  <div class="rsep"></div>
-  <div class="rsep"></div>
-  <div class="rsep"></div>
-  <div class="rsep"></div>
-  <div class="rsep"></div>
-  <div class="rsep"></div>
-</div>
-
-<div class="green-wrapper fixed-link-container">
-  <?php get_component('fixed-link', array('text' => 'xdxdxd', 'link' => 'a'));?>
-  <div class="rsep"></div>
-  <div class="rsep"></div>
-  <div class="rsep"></div>
-  <div class="rsep"></div>
-  <div class="rsep"></div>
   <div class="rsep"></div>
 </div>
 
