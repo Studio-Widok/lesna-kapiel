ea3e1b34e237aa61eeaa6aeeca2f6d8ca4e61306
wojciechlasak <wojciech.lasak@outlook.com>
2020-08-27 16:27:02 +0200

    added more air

--- single-apartment.php
+++ single-apartment.php
@@ -3,11 +3,13 @@
 ?>
 <div class="green-wrapper">
   <div class="rsep"></div>
+  <div class="rsep"></div>
   <?php get_part('slider-gallery', [
       'title'   => get_the_title(),
       'text'    => get_field('text'),
       'gallery' => get_field('gallery'),
   ]);?>
+  <div class="rsep"></div>
   <div class="rsep"></div>
 </div>
 <?php get_footer();?>
