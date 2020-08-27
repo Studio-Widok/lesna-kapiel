d991416c1306868a6fae7b3a1645866bf5186bff
wojciechlasak <wojciech.lasak@outlook.com>
2020-08-27 16:27:23 +0200

    deleted duplicate class

--- components/slider-arrows.php
+++ components/slider-arrows.php
@@ -4,14 +4,13 @@
 
 <div class="slider-arrows">
   <div class="arrow-left">
-    <svg xmlns="http://www.w3.org/2000/svg" viewBox="-8.5 0 27 27"
-      class="arrow-left">
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="-8.5 0 27 27">
       <path d="M5 0L5 17" stroke-width="1.5" stroke="#fff" />
       <path d="M5 27L1.5 17L8.5 17z" stroke-width="0" fill="#fff" />
     </svg>
   </div>
   <div class="arrow-right">
-    <svg xmlns="http://www.w3.org/2000/svg" viewBox="-8.5 0 27 27" class="">
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="-8.5 0 27 27">
       <path d="M5 0L5 17" stroke-width="1.5" stroke="#fff" />
       <path d="M5 27L1.5 17L8.5 17z" stroke-width="0" fill="#fff" />
     </svg>
