9d4e2a3c1a5b3b94d8c0fea9f0021d23c88db522
wojciechlasak <wojciech.lasak@outlook.com>
2020-08-28 13:51:58 +0200

    fixed arrows

--- components/slider-arrows.php
+++ components/slider-arrows.php
@@ -10,9 +10,11 @@
     </svg>
   </div>
   <div class="arrow-right">
+    <?php if ($next_arrow_text) {?>
     <div class="arrow-text">
       <?=$next_arrow_text?>
     </div>
+    <?php }?>
     <svg xmlns="http://www.w3.org/2000/svg" viewBox="-8.5 0 27 27">
       <path d="M5 0L5 17" stroke-width="1.5" stroke="#fff" />
       <path d="M5 27L1.5 17L8.5 17z" stroke-width="0" fill="#fff" />
