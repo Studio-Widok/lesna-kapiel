7c819c8a8b032ca2abf2772b4215f26fd350969e
wojciechlasak <wojciech.lasak@outlook.com>
2020-08-31 13:41:25 +0200

    added wp for 2-col-pic and front-page

--- parts/2-col-no-pic.php
+++ parts/2-col-no-pic.php
@@ -1,41 +1,26 @@
+<?php
+  $title  = $title ?? '';
+  $text   = $text ?? '';
+  $button = $button ?? false;
+?>
+
 <div class="content col-2-no-pic">
   <div class="flex">
     <div class="col2 column">
       <div class="text">
-        <p><strong>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus,
-            eos
-            unde reiciendis nesciunt rerum natus placeat id aperiam suscipit
-            voluptatibus, ad exercitationem quasi veritatis nihil minus delectus
-            necessitatibus ut
-            dolore? Blanditiis fuga pariatur officiis architecto. At eaque quasi
-            iure quos?
-          </strong></p>
-      </div>
-      <div class="rmin"></div>
-      <div class="text">
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit.
-          Praesentium porro illo harum doloremque corrupti cupiditate ut,
-          obcaecati unde omnis atque explicabo aperiam sed distinctio amet
-          mollitia magnam exercitationem facere impedit optio eum expedita
-          eligendi
-          iusto labore. Ipsam illum tempora quibusdam labore veritatis quisquam
-          laboriosam quidem, magnam velit, in natus debitis. Distinctio esse
-          alias
-          earum recusandae aliquam corporis optio blanditiis, ducimus
-        </p>
+        <?=$text?>
       </div>
       <div class="rmin"></div>
       <div class="rmin"></div>
-
-      <button>
-        Więcej
-      </button>
+      <a href="<?=$button['link']?>">
+        <button>
+          <?=$button['text']?>
+        </button>
+      </a>
     </div>
     <div class="col2 column">
       <div class="big-title handwrite">
-        shinrin<br>-&nbsp;yoku
+        <?=$title?>
       </div>
     </div>
 
