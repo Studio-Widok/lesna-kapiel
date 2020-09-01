e3fe2c8d94ed796db3aebd2401c16d4bfdbd4d49
wojciechlasak <wojciech.lasak@student.uj.edu.pl>
2020-09-01 17:05:34 +0200

    full-width-image changed, nasza idea added foote

--- parts/full-width-image.php
+++ parts/full-width-image.php
@@ -1,5 +1,7 @@
 <?php
   $image = $image ?? [];
+  $isOverlay = $isOverlay ?? false;
+  $isContactInfo = $isContactInfo ?? true;
 
   if (empty($image)) {
     return;
@@ -7,4 +9,10 @@
 ?>
 
 <div class="full-width-image cake cake-16-9"
-  style="background-image: url('<?=$image['sizes']['large']?>');"></div>
+  style="background-image: <?php if($isOverlay) echo 'linear-gradient(rgba(0, 0, 0, 0.6),'?> url('<?=$image['sizes']['large']?>');">
+  <?php if($isContactInfo): ?>
+  <div>
+    <?php get_part('contact-info'); ?>
+  </div>
+  <?php endif ?>
+</div>
--- templates/t-nasza-idea.php
+++ templates/t-nasza-idea.php
@@ -1,7 +1,20 @@
 <?php /*Template Name: nasza idea*/
   get_header();
+  $footer = get_field('footer', 2);
 ?>
-
+<div class="green-wrapper">
+  <?php
+    get_part('footer-video', array(
+      'source' => $footer['video'],
+      'text'   => $footer['text']));
+  ?>
+  <div class="rsep"></div>
+  <?php get_part('contact-info');?>
+  <div class="rsep"></div>
+</div>
 <?php get_part('map-block')?>
+<div class="green-wrapper green-wrapper-footer">
+  <div class="rmin"></div>
+</div>
 
 <?php get_footer();?>
