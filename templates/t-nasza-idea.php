71a42f48d63704ab0f2c761e17f42c914ef8956f
wojciechlasak <wojciech.lasak@outlook.com>
2020-09-02 15:58:43 +0200

    added content

--- templates/t-nasza-idea.php
+++ templates/t-nasza-idea.php
@@ -1,14 +1,35 @@
 <?php /*Template Name: nasza idea*/
   get_header();
   $footer = get_field('footer', 2);
+  $top    = get_field('top')['top'];
   get_part('nav');
+  get_part('top', array(
+    'show_title' => true,
+    'title'      => $top['title'],
+    'bg'         => $top['top_image'],
+    'text_align' => $top['align'],
+  ));
 ?>
+<div class="green-wrapper">
+  <div class="rsep"></div>
+  <?php get_part('text-full', array('text' => $top['text']));?>
+  <div class="rsep"></div>
+  <div class="rsep"></div>
+  <div class="rsep"></div>
+</div>
 <div class="green-wrapper">
   <?php
     get_part('footer-video', array(
       'source' => $footer['video'],
       'text'   => $footer['text']));
   ?>
+  <div class="rsep"></div>
+  <?php $section = get_field('section');
+    get_part('2-col-with-pic', [
+      'image'      => $section['image'],
+      'text'       => $section['text'],
+      'alt_layout' => true,
+  ]);?>
   <div class="rsep"></div>
 </div>
 <?php get_part('full-width-image', [
