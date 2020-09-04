6611009267886ef6edbf5e47adcebfa456194a21
wojciechlasak <wojciech.lasak@outlook.com>
2020-09-04 14:07:23 +0200

    adjust mobile

--- parts/slider-with-bullets.php
+++ parts/slider-with-bullets.php
@@ -6,12 +6,12 @@
 <?php if (!empty($slides)) {?>
 <div class="slider-with-bullets" id="slider-with-bullets-<?=$__index?>">
   <div class="content">
-    <div class="slider-bullets">
+    <div class="slider-bullets column-768">
       <div class="small-title"><?=$title?></div>
       <div class="rmin"></div>
-      <div class="bullets-container">
+      <div class="bullets-container flex">
         <?php for ($i = 0; $i < count($slides); $i++) {?>
-        <div class="bullet">
+        <div class="bullet col-2">
           <div class="flex flex-align-center">
             <img src="<?=$slides[$i]['icon']['sizes']['medium']?>"
               class="slide-icon">
@@ -20,18 +20,20 @@
         </div>
         <?php }?>
       </div>
+      <div class="r less-768"></div>
     </div>
     <div class="slider">
       <?php for ($i = 0; $i < count($slides); $i++) {?>
       <div class="single-slide">
-        <div class="flex flex-align-end">
+        <div class="flex flex-wrap flex-768 flex-align-end">
           <div class="col2 column">
             <div class="cake cake-3-4"
               style="background-image: url('<?=$slides[$i]['image']['sizes']['medium']?>');">
               <?php get_component('cake-frame');?>
             </div>
           </div>
-          <div class="col2 column">
+          <div class="col2 column single-slide-desc">
+            <div class="r less-1050"></div>
             <div class="flex flex-align-center">
               <img src="<?=$slides[$i]['icon']['sizes']['medium']?>"
                 class="slide-icon">
