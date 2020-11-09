7d105bae22d91103e976d0700a05470a3ef0478c
weterynarzfred <weterynarzfred@gmail.com>
2020-11-09 11:15:21 +0100

    fix scratch colors

--- front-page.php
+++ front-page.php
@@ -6,41 +6,41 @@
   $slider         = get_field('slider');
   $footer         = get_field('footer');
   get_part('nav');
-  get_part('top', array(
+  get_part('top', [
     'show_logo'         => true,
     'bg'                => get_field('top_image'),
     'isShowReservation' => true,
-  ));
+  ]);
 ?>
 
 <div class="green-wrapper">
   <div class="fixed-link-container">
-    <?php get_component('fixed-link', array('text' => get_the_title(pll_get_post(100)), 'link' => get_the_permalink(pll_get_post(100))));?>
+    <?php get_component('fixed-link', ['text' => get_the_title(pll_get_post(100)), 'link' => get_the_permalink(pll_get_post(100))]);?>
     <div class="rsep"></div>
     <div class="rsep more-1200"></div>
     <?php get_part('2-col-no-pic', [
         'title'  => $firstSection['title'],
         'text'   => $firstSection['text'],
-        'button' => array(
+        'button' => [
           'text' => $firstSection['link']['text'],
           'link' => get_link_url($firstSection['link']),
-        ),
+        ],
     ])?>
     <div class="rsep"></div>
   </div>
   <div class="fixed-link-container">
     <?php $termId = get_term_by('term_taxonomy_id', pll_get_term(get_term_id('villa')));
-    get_component('fixed-link', array('text' => pll__('see_apartments'), 'link' => get_tag_link($termId)));?>
+    get_component('fixed-link', ['text' => pll__('see_apartments'), 'link' => get_tag_link($termId)]);?>
     <div class="rsep"></div>
     <?php for ($i = 0; $i < count($sections); $i++) {?>
-<?php get_part('2-col-with-pic', [
+    <?php get_part('2-col-with-pic', [
     'image'         => $sections[$i]['image'],
     'title'         => $sections[$i]['title'],
     'text'          => $sections[$i]['text'],
-    'button'        => array(
+    'button'        => [
       'text' => $sections[$i]['link']['text'],
       'link' => get_link_url($sections[$i]['link']),
-    ),
+    ],
     'alt_layout'    => true,
     'pic_right'     => $i % 2,
     'isTextOverlap' => $i === 1,
@@ -65,14 +65,14 @@
   ]);?>
   <div class="rsep"></div>
   <?php get_component('reservation')?>
-<?php get_part('full-width-image', ['image' => $footer['image'], 'ratio' => 16 / 9]);?>
+  <?php get_part('full-width-image', ['image' => $footer['image'], 'ratio' => 16 / 9]);?>
 </div>
 <div class="green-wrapper">
   <?php
     $footer = get_field('footer', 2);
-    get_part('footer-video', array(
+    get_part('footer-video', [
       'source' => $footer['video'],
-      'text'   => $footer['text']));
+      'text'   => $footer['text']]);
   ?>
   <div class="rsep"></div>
   <?php get_part('contact-info');?>
