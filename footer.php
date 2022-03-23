e16fa835e9d0db6e5aa9777e9113e1b6cade7a61
weterynarzfred <weterynarzfred@gmail.com>
2022-03-23 14:04:32 +0100

    footer

--- footer.php
+++ footer.php
@@ -1,3 +1,16 @@
+<footer class="dark-wrapper column-outer">
+  <div class="rmin"></div>
+  <div class="flex rel uppercase">
+    <a href="<?=get_the_permalink(3)?>">
+      <div class="foot-link column-inner"><?=get_the_title(3)?></div>
+    </a>
+    <a href="<?=get_the_permalink(813)?>">
+      <div class="foot-link column-inner"><?=get_the_title(813)?></div>
+    </a>
+    <div class="foot-link column-inner">copyrights: leśna kąpiel</div>
+  </div>
+  <div class="r"></div>
+</footer>
 <script>
 const baseUrl = '<?=get_template_directory_uri()?>';
 </script>
