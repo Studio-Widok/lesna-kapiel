aa65247248970b91df04ccd1b144e652a71a2a60
weterynarzfred <weterynarzfred@gmail.com>
2022-04-28 14:14:21 +0200

    add google tag manager

--- header.php
+++ header.php
@@ -31,7 +31,31 @@
   <script type="text/javascript"
     src="https://panel.hotres.pl/public/api/hotres_v4.js"></script>
 
+  <!-- Google Tag Manager -->
+  <script>
+  (function(w, d, s, l, i) {
+    w[l] = w[l] || [];
+    w[l].push({
+      'gtm.start': new Date().getTime(),
+      event: 'gtm.js'
+    });
+    var f = d.getElementsByTagName(s)[0],
+      j = d.createElement(s),
+      dl = l != 'dataLayer' ? '&l=' + l : '';
+    j.async = true;
+    j.src =
+      'https://www.googletagmanager.com/gtm.js?id=' + i + dl;
+    f.parentNode.insertBefore(j, f);
+  })(window, document, 'script', 'dataLayer', 'GTM-P4PS862');
+  </script>
+  <!-- End Google Tag Manager -->
+
   <?php wp_head();?>
 </head>
 
 <body <?php body_class();?>>
+  <!-- Google Tag Manager (noscript) -->
+  <noscript><iframe
+      src="https://www.googletagmanager.com/ns.html?id=GTM-P4PS862" height="0"
+      width="0" style="display:none;visibility:hidden"></iframe></noscript>
+  <!-- End Google Tag Manager (noscript) -->
