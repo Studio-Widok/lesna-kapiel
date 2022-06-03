f2d35c3dd5d7f9ab1a862e5755d91a3ab27e4c1d
weterynarzfred <weterynarzfred@gmail.com>
2022-06-03 18:24:18 +0200

    bump script versions

--- include/enqueue.php
+++ include/enqueue.php
@@ -2,6 +2,6 @@
 
 add_action('wp_enqueue_scripts', function () {
   $url = get_template_directory_uri() . '/dist/';
-  wp_enqueue_style('base', $url . 'main.css', [], 1.6);
-  wp_enqueue_script('bundle', $url . 'main.js', [], 1.0, true);
+  wp_enqueue_style('base', $url . 'main.css', [], '1.7');
+  wp_enqueue_script('bundle', $url . 'main.js', [], '1.1', true);
 });
