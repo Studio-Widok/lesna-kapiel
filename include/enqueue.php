e0ca56f74be8e05d24c6381f463110fb2b362785
weterynarzfred <weterynarzfred@gmail.com>
2022-06-12 17:17:10 +0200

    switch to production

--- include/enqueue.php
+++ include/enqueue.php
@@ -2,6 +2,6 @@
 
 add_action('wp_enqueue_scripts', function () {
   $url = get_template_directory_uri() . '/dist/';
-  wp_enqueue_style('base', $url . 'main.css', [], '1.9');
+  wp_enqueue_style('base', $url . 'main.css', [], '1.10');
   wp_enqueue_script('bundle', $url . 'main.js', [], '1.2', true);
 });
