3a0c2d761bef50cdb959c4a7ab64b8184d101483
weterynarzfred <weterynarzfred@gmail.com>
2022-08-03 12:42:18 +0200

    styling

--- parts/language-switcher.php
+++ parts/language-switcher.php
@@ -1,12 +1,16 @@
-<span class="language-switcher nav-link">
+<span class="language-switcher">
   <?php
     $langs = pll_the_languages([
       'raw'          => true,
       'hide_current' => true,
     ]);
     foreach ($langs as $lang) {
+      // TEMP disable german language for now
+      if (!is_user_logged_in() && $lang['slug'] === 'de') {
+        continue;
+      }
     ?>
-  <a href="<?=$lang['url']?>" class="lang-link uppercase">
+  <a href="<?=$lang['url']?>" class="lang-link nav-link uppercase">
     <span><?=$lang['slug']?></span>
   </a>
   <?php }?>
