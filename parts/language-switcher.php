63fe7f88b9073a0ae6912a7214d67b3eb86409f3
weterynarzfred <weterynarzfred@gmail.com>
2022-08-19 12:01:32 +0200

    language switcher

--- parts/language-switcher.php
+++ parts/language-switcher.php
@@ -1,17 +1,16 @@
-<span class="language-switcher">
-  <?php
-    $langs = pll_the_languages([
-      'raw'          => true,
-      'hide_current' => true,
-    ]);
-    foreach ($langs as $lang) {
-      // TEMP disable german language for now
-      if (!is_user_logged_in() && $lang['slug'] === 'de') {
-        continue;
-      }
-    ?>
-  <a href="<?=$lang['url']?>" class="lang-link nav-link uppercase">
-    <span><?=$lang['slug']?></span>
-  </a>
-  <?php }?>
-</span>
+<div class="language-dropdown">
+  <div class="lang-title uppercase"><?=pll_current_language()?></div>
+  <div class="lang-list">
+    <?php
+      $langs = pll_the_languages([
+        'raw'          => true,
+        'hide_current' => true,
+      ]);
+      foreach ($langs as $lang) {
+      ?>
+    <a href="<?=$lang['url']?>" class="lang-link nav-link uppercase">
+      <span><?=$lang['slug']?></span>
+    </a>
+    <?php }?>
+  </div>
+</div>
--- src/js/nav.js
+++ src/js/nav.js
@@ -11,3 +11,25 @@ $('.nav-link-scroll').on('click', event => {
   const target = $(href).offset().top - 100;
   window.scrollTo({ top: target, behavior: 'smooth' });
 });
+
+// language switcher
+function langClose(event) {
+  if (event?.fromLanguageDropdown) return;
+  langDropdownElement.removeClass('open');
+  document.removeEventListener('click', langClose);
+}
+
+const langDropdownElement = $('.language-dropdown');
+
+$('.language-dropdown .lang-title').on('click', event => {
+  if (langDropdownElement.hasClass('open')) {
+    langClose();
+  } else {
+    langDropdownElement.addClass('open');
+    document.addEventListener('click', langClose);
+  }
+});
+
+langDropdownElement[0].addEventListener('click', event => {
+  event.fromLanguageDropdown = true;
+});
